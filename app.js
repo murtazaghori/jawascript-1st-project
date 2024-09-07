b1a4491b7c57d5b1a0ab8dd41c1da67ea7b0b5c4
murtaza <murtazakhanghori@gmail.com>
2024-09-07 10:58:41 -0700

    all update

---
+++ app.js
@@ -0,0 +1,175 @@
+// ch:1
+// Q:1
+// alert("welcome visitor");
+
+// Q:2
+// alert("Error!Please enter a valid Password")
+
+// Q:3
+// alert("Welcome to Js Land ..\n Happy Coding!")
+
+// Q:4
+// alert("Welcome To js Land")
+// alert("Prevent this from page extra dialogue")
+
+// Q:5
+// console.log("Hello .... I can Run JS through my Web Broswer Console")
+
+// Ch:End
+
+// Ch:2
+// Q:1
+// var username
+
+// Q:2
+// var username ="my Name"
+
+// Q:3
+// p:a
+// var name ="murtaza";
+// p:b
+// name ="ghori"
+// P:c
+// alert("message")
+
+// Q:4
+// alert("murtaza");
+// alert("18years");
+// alert(my name is murtaza ghori);
+
+// Q:5
+// alert("PIZZA \n PIZZ \n PIZ \n Pi \n P");
+
+// Q:6
+// alert("mynameis@gamil.com");
+
+// Q:7
+// alert("i am tried to leran english of the world \n the book is empty.");
+
+// Q:8
+// document.write("welcome to my website");
+
+// Q:9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ");
+
+// ch:3
+// Q:1
+// var age; 
+// age ="i am 18 years old"
+// alert("age");
+ 
+// Q:2
+// var time;
+// time="You have visited this site 14 times"
+// alert(time)
+
+// Q:3
+// document.write("My birth year is 2006 <br> Data type of my declared variable is")
+
+// Q:4
+// p:a
+//  var visitorName = "John Doe"; 
+//  P:b 
+//  var productTitle = "T-shirt";
+//  P:c  
+//  var quantity = 5;  
+//  var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+//  document.write(message);
+
+// CH:4
+// Q:1
+// var visitorName = "John Doe", productTitle = "T-shirt", quantity = 5;
+
+// Q:2
+// 2. Legal & Illegal Variable Names
+// Legal Variable:
+
+//  1. userName:  
+//  2. _age:  
+//  3. $price:  
+//  4. totalAmount:  
+//  5. user1:
+
+//  Illegal Variable 
+// 1. 1user:
+// 2. user-name: 
+// 3. user name: 
+// 4. @username:
+// 5. var:
+
+
+// //  Q:3
+
+// // P:a
+//  document.write("<h1>Rules for naming JS variables</h1>");
+// //  P:b
+//  document.write("<p>Variable names can only contain letters, numbers, underscores, and dollar signs. Example: $my_1stVariable.</p>");
+// //  P:c
+//  document.write("<p>Variables must begin with a letter, underscore, or dollar sign. For example $name, _name or name.Example: $name, _name or name</p>");
+// //  P:d
+//  document.write("<p>Variable names are case sensitive.</p>");
+// //  P:e
+//  document.write("<p>Variable names should not be JavaScript keywords.</p>");
+ 
+
+
+                // ASSIGNMEMT QUESTION:
+                // CH:1
+
+// Q:1. Alert these following (individually):
+// P(i):
+ 
+// alert("murtaza");
+
+//  // P(i):
+// alert("ghori");
+
+// // P(ii):
+// alert("example@gamil.com");
+
+// // P(iii):
+// alert("0321*******");
+
+// // P(iv):
+// alert("........");
+
+// Q:2
+// alert("You're learning JavaScript!");
+
+// Q:3
+// alert("Hello, welcome to our website!");
+
+// CH:2
+// Q:1
+// Camel Case:
+// var userAge =30;
+
+// Q:2
+// var message
+// message ="hello developer";
+
+// Q:3
+// var name ="dreamteam";
+// alert("name");
+
+// Q:4
+// var bestman ="Charlie";
+// bestman ="Babbage";
+
+// CH:3
+// Q:1
+// var num;
+// // Q:2
+// num = 144;
+// // Q:3
+// num = "9"
+
+// Q:6
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+
+// "(ordertotal broswer =110)"
+
+
+ 
